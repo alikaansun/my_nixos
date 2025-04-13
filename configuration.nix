630872712a5764e0b2175f618be1e40a55c5107c
alik <asunnetcoglu@gmail.com>
2025-04-13 20:02:41 +0200

    removed recursive homemanager input

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
+      # inputs.home-manager.nixosModules.default
     ];
 
   nixpkgs.config.allowUnfree = true;
