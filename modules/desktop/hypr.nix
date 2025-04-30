982546bacc49e6bf5b247472ca17bc86945dc8e9
alik <asunnetcoglu@gmail.com>
2025-04-30 21:57:39 +0200

    back tokde

--- modules/desktop/hypr.nix
+++ modules/desktop/hypr.nix
@@ -10,6 +10,6 @@
   package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   # make sure to also set the portal package, so that they are in sync
   portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-};
+  };  
 
 }
