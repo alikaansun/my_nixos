d0befba14673c2781d4181b09a42d991ebe463bb
alik <asunnetcoglu@gmail.com>
2025-06-13 13:10:32 +0200

    laptop files

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,148 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix #Dont disable it
+      #CUSTOM-MODULES
+      ../../modules/bootloader.nix #bootloader #Dont disable it
+      ../../modules/locale.nix #Dont disable it
+      ../../modules/gc.nix #garbage collection and store opt
+#       ../../modules/extrastorage.nix #extra storage
+      ../../modules/gaming.nix
+#       ../../modules/virtualisation.nix
+#       ../../modules/networking.nix
+
+      #./modules/localai.nix
+      #DESKTOP-MODULES
+      ../../modules/desktop/kde.nix
+      # ./modules/desktop/hypr.nix
+      # ./modules/desktop/xfce.nix
+      # ./modules/desktop/gnome.nix
+
+      # ./modules/desktop/stylix.nix
+      # inputs.home-manager.nixosModules.default
+    ];
+
+
+
+
+
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+
+  networking.hostName = "laptop"; # Define your hostname.
+
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  #hardware.graphics = {
+  #  enable = true;
+  #  enable32Bit = true;
+  #};
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.alik = {
+    # hashedPasswordFile = "/etc/passwdfile";
+    isNormalUser = true;
+    description = "alik";
+    extraGroups = [ "networkmanager" "wheel" "input" "vboxusers" "libvirt" ];
+    # packages = with pkgs; [];
+  };
+  # home-manager
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "alik" = import ./home.nix;
+    };
+    backupFileExtension = "backup";
+
+  };
+
+
+  environment.systemPackages = with pkgs; [
+   wget
+   git
+   nvd#nixos package version diff tool
+   nixd#nix language server
+   alejandra #nix language server
+   nerd-fonts.fira-code
+   nerd-fonts.meslo-lg
+   chromium
+   unrar
+   gparted
+   nvtopPackages.full
+   kdePackages.filelight
+   eduvpn-client
+
+	#];
+#})
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 57621 ];
+  networking.firewall.allowedUDPPorts = [ 5353 ];
+  # Enable automatic login for the user.services.displayManager.autoLogin
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "alik";
+  #Programs
+
+    # List services that you want to enable:
+
+
+  #trezord adds required udev rules to start the bridge
+  services.trezord.enable = true;
+
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ hosts/laptop/home.nix
@@ -0,0 +1,126 @@
+{ config, pkgs,
+lib ,
+programs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "alik";
+  home.homeDirectory = "/home/alik";
+  nixpkgs.config.allowUnfree = true;
+  home.enableNixpkgsReleaseCheck=false;
+
+  imports =
+    [
+      # ./modules/desktop/hyprland.nix
+      ../../modules/terminal.nix
+      ../../modules/git.nix
+#       ../../modules/creative.nix
+
+    ];
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    keepassxc
+    trezor-suite
+    thunderbird
+    vscode
+    obsidian
+    fastfetch
+    klayout
+    discord
+    vlc
+    parted
+    nextcloud-client
+    tor-browser
+    libreoffice-qt6
+    ffmpeg
+    zotero
+    obs-studio
+    spotify
+    foliate #ebook
+    rustdesk
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  #virt-manager with wayland requires a gdk cursor to be set
+  #In order to run on Wayland, virt-manager must be ran under XWayland with `$ GDK_BACKEND=x11 virt-manager` or a gdk cursor must be set.
+  #An example of setting a gdk cursor with home-manager is as follows:
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.vanilla-dmz;
+    name = "Vanilla-DMZ";
+  };
+
+  # home.backupFileExtension = "backup";
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".config/kdeglobals".source = ./modules/.config/kdeglobals;
+    # ".config/kwinrc".source = ./modules/.config/kwinrc;
+    # ".config/kglobalshortcutsrc".source = ./modules/.config/kglobalshortcutsrc;
+    # ".config/kwinrulesrc".source = ./modules/.config/kwinrulesrc;
+    # ".config/kcminputrc".source = ./modules/.config/kcminputrc;
+
+
+    # ".config/kcminputrc" = {
+    #   source = ./modules/.config/kcminputrc;
+    #   force = true;
+    # };
+  # Add more as needed
+
+
+
+    # Add more as needed
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+    # recursive=true;
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/alik/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
