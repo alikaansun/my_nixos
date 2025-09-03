5a8d1b41bcf4fb53b5cc79feaacd31b62085d1cc
alik <asunnetcoglu@gmail.com>
2025-09-03 20:23:23 +0200

    save before deleting seafile

--- modules/services/seafile.nix
+++ modules/services/seafile.nix
@@ -1,35 +1,56 @@
-{vars,config,pkgs, ... }:
+{vars, config, pkgs, ... }:
 
 {
   config = {
-
-  sops.secrets = {
-    seafile_email = {};
-    seafile_adminpw = {};
+    sops.secrets = {
+      seafile_email = {};
+      seafile_adminpw = {};
     };
 
-  services.seafile = {
-    enable = true;
-    adminEmail = config.sops.secrets.seafile_email.path;
-    initialAdminPassword = config.sops.secrets.seafile_adminpw.path;
-    user="seafile";
-    group="seafile";
-    # ccnetSettings.general.service_url = "https://${vars.seafile.hostName}";
-    seafileSettings = {
-      fileserver = {
-        port = "ipv4:${vars.seafile.port}";
-        host = vars.seafile.IP;
+    services.mysql = {
+      enable = true;
+      package = pkgs.mariadb;
+      settings = {
+        mysqld = {
+          innodb_file_per_table = 1;
+          innodb_buffer_pool_size = "128M";
+        };
+      };
+      initialDatabases = [
+        { name = "seafile-db"; }
+        { name = "seahub-db"; }
+        { name = "ccnet-db"; }
+      ];
+      ensureUsers = [
+        {
+          name = "seafile";
+          ensurePermissions = {
+            "seafile-db.*" = "ALL PRIVILEGES";
+            "seahub-db.*" = "ALL PRIVILEGES";
+            "ccnet-db.*" = "ALL PRIVILEGES";
+          };
+        }
+      ];
+    };
+
+    services.seafile = {
+      enable = true;
+      adminEmail = config.sops.secrets.seafile_email.path;
+      initialAdminPassword = config.sops.secrets.seafile_adminpw.path;
+      ccnetSettings.General.SERVICE_URL = "https://${vars.seafile.hostName}";
+      seafileSettings = {
+        fileserver = {
+          port = vars.seafile.port;
+          host = vars.seafile.IP;
+        };
       };
     };
-    # dataDir = "";
-  };
+
     networking.firewall.allowedTCPPorts = [ vars.seafile.port ];
 
     services.caddy.virtualHosts."${vars.seafile.hostName}".extraConfig = ''
-    tls internal
-    reverse_proxy ${vars.seafile.IP}:${toString vars.seafile.port}
-  '';
-
-    
+      tls internal
+      reverse_proxy 127.0.0.1:${toString vars.seafile.port}
+    '';
   };
 }
