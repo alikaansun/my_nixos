a571d792669d7675c87fba18cc6927579039eea8
alik <asunnetcoglu@gmail.com>
2026-01-26 22:24:48 +0100

    kanata edited for mac possible aarch laptop

--- modules/services/kanata.nix
+++ modules/services/kanata.nix
@@ -1,9 +1,44 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 
 let
   cfg = config.services.mykanata;
+  
+  # Platform detection
+  isDarwin = pkgs.stdenv.isDarwin;
+  isAarch64 = pkgs.stdenv.hostPlatform.isAarch64;
+  
+  # M-chip Mac detection
+  isMmac = isDarwin && isAarch64;
+  
+  # Shared Kanata configuration
+  kanataConfig = ''
+    (defsrc
+      caps a s d f j k l ;
+    )
+    (defvar
+      tap-time 150
+      hold-time 200
+    )
+
+    (defalias
+      escctrl (tap-hold 100 100 esc lctl)
+      a (multi f24 (tap-hold $tap-time $hold-time a lsft)) 
+      s (multi f24 (tap-hold $tap-time $hold-time s lalt)) 
+      d (multi f24 (tap-hold $tap-time $hold-time d lmet)) 
+      f (multi f24 (tap-hold $tap-time $hold-time f lctl)) 
+      j (multi f24 (tap-hold $tap-time $hold-time j rctl)) 
+      k (multi f24 (tap-hold $tap-time $hold-time k rmet)) 
+      l (multi f24 (tap-hold $tap-time $hold-time l ralt)) 
+      ; (multi f24 (tap-hold $tap-time $hold-time ; rsft)) 
+    )
+
+    (deflayer base
+      @escctrl @a @s @d @f @j @k @l @;
+    )
+  '';
+
 in
 
 {
@@ -21,62 +56,52 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable (mkMerge [
+    # Linux-specific configuration
+    (mkIf (!isDarwin) {
+      boot.kernelModules = [ "uinput" ];
+      users.groups.uinput = { };
 
-    boot.kernelModules = [ "uinput" ];
-    users.groups.uinput = { };
+      services.udev.extraRules = ''
+        KERNEL=="uinput", GROUP="uinput", MODE="0660", OPTIONS+="static_node=uinput"
+      '';
 
-    services.udev.extraRules = ''
-      KERNEL=="uinput", GROUP="uinput", MODE="0660", OPTIONS+="static_node=uinput"
-    '';
-
-    # Ensure proper permissions on boot (optional; udev rule is usually enough)
-    systemd.tmpfiles.rules = [
-      "c /dev/uinput 0660 root uinput"
-    ];
-
-    systemd.services."kanata-internalKeyboard".serviceConfig = {
-      # DynamicUser = false;
-      User = "alik";
-      SupplementaryGroups = [
-        "input"
-        "uinput"
+      systemd.tmpfiles.rules = [
+        "c /dev/uinput 0660 root uinput"
       ];
-    };
 
-    services.kanata = {
-      enable = true;
-      keyboards = {
-        internalKeyboard = {
-          devices = [ cfg.deviceName ];
-          extraDefCfg = "process-unmapped-keys yes";
-          config = ''
-            (defsrc
-              caps a s d f j k l ;
-            )
-            (defvar
-              tap-time 150
-              hold-time 200
-            )
+      systemd.services."kanata-internalKeyboard".serviceConfig = {
+        User = "alik";
+        SupplementaryGroups = [
+          "input"
+          "uinput"
+        ];
+      };
 
-            (defalias
-              escctrl (tap-hold 100 100 esc lctl)
-              a (multi f24 (tap-hold $tap-time $hold-time a lsft)) 
-              s (multi f24 (tap-hold $tap-time $hold-time s lalt)) 
-              d (multi f24 (tap-hold $tap-time $hold-time d lmet)) 
-              f (multi f24 (tap-hold $tap-time $hold-time f lctl)) 
-              j (multi f24 (tap-hold $tap-time $hold-time j rctl)) 
-              k (multi f24 (tap-hold $tap-time $hold-time k rmet)) 
-              l (multi f24 (tap-hold $tap-time $hold-time l ralt)) 
-              ; (multi f24 (tap-hold $tap-time $hold-time ; rsft)) 
-            )
-
-            (deflayer base
-              @escctrl @a @s @d @f @j @k @l @;
-            )
-          '';
+      services.kanata = {
+        enable = true;
+        keyboards = {
+          internalKeyboard = {
+            devices = [ cfg.deviceName ];
+            extraDefCfg = "process-unmapped-keys yes";
+            config = kanataConfig;
+          };
         };
       };
-    };
-  };
+    })
+    # M-chip Mac (aarch64-darwin) configuration
+    (mkIf isMmac {
+      environment.etc."kanata/config.kbd".text = kanataConfig;
+      
+      launchd.user.agents.kanata = {
+        command = "${pkgs.kanata}/bin/kanata -c /etc/kanata/config.kbd";
+        serviceConfig = {
+          KeepAlive = true;
+          RunAtLoad = true;
+          StandardOutPath = "/tmp/kanata.out.log";
+          StandardErrorPath = "/tmp/kanata.err.log";
+        };
+      };
+    })
+  ]);
 }
