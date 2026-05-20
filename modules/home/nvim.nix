079e88b1d056d3212e61090f0e846716df69c2ac
alik <asunnetcoglu@gmail.com>
2026-05-20 22:57:15 +0200

    nvim sup'd

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -6,6 +6,7 @@
 
       home.packages = with pkgs; [
         tree-sitter
+
       ];
 
       programs.nvf = {
@@ -14,22 +15,84 @@
         settings = {
           ##################
           vim = {
-            viAlias = false;
+            # --- 1. Core & Globals ---
+            viAlias = true;
             vimAlias = true;
+
+            # Set Space as the leader key
+            globals = {
+              mapleader = " ";
+              maplocalleader = " ";
+            };
+            
+            # --- 2. UI & Theming ---
             theme = {
               enable = true;
               name = "gruvbox";
               style = "dark";
             };
+
+            ui = {
+              colorizer.enable = true; # Highlight color codes (e.g., #FFFFFF)
+              borders = {
+                enable = true;
+                plugins = {
+                  lsp-signature.enable = true;
+                };
+              };
+            };
+
+            statusline.lualine.enable = true;
+            
+            # Show open buffers as tabs for easy switching in the same session
+            tabline.nvimBufferline.enable = true;
+
+            # --- 3. Navigation & Terminal ---
+            telescope.enable = true; # Fuzzy finder for files, ripgrep, etc.
+
+            terminal.toggleterm = {
+              enable = true;
+              lazygit.enable = true; 
+              mappings = {
+                open = "<c-t>";
+              };
+            };
+
+            # --- 4. Utilities & Git ---
+            binds.whichKey.enable = true; # Keybind helper popups
+            git.enable = true;
+
+            # --- 5. LSP, Autocomplete & Core Language Features ---
+            autocomplete.nvim-cmp.enable = true;
+
+            treesitter = {
+              enable = true;
+            };
+
+            lsp = {
+              enable = true;
+              # lsp-signature is EXCELLENT! 
+              # It shows a popup with the function arguments you are typing.
+              lspSignature.enable = true; 
+            };
+
+            # --- 6. Specific Language Support ---
             languages = {
-              enableTreesitter = true;
-              nix.enable = true;
+              enableFormat = true;
+              enableTreesitter = true; # Relies on the core treesitter enabled above
+              enableExtraDiagnostics = true;
+
+              nix = {
+                enable = true;
+                format = {
+                  type = "treefmt";
+                };
+              };
+              
               bash.enable = true;
               python.enable = true;
               clang.enable = true;
             };
-            statusline.lualine.enable = true;
-            telescope.enable = true;
           };
           ############
         };
